ca3ab0e820dfb88e9f3baedc80f4bddf3e9b08ae
“Liam-YorkRobertson” <liamyorkdeonrobertson@gmail.com>
2023-12-15 00:16:55 +0000

    SQL script that creates a stored procedure ComputeAverageScoreForUser that computes and store the average score for a student

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -4,5 +4,5 @@ DELIMITER $$
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
 UPDATE users
-SET average_score = IFNULL((SELECT AVG(score) FROM corrections WHERE user_id = user_id), 0) WHERE id = user_id;
+SET average_score = IFNULL((SELECT AVG(score) FROM corrections WHERE corrections.user_id = user_id), 0) WHERE id = user_id;
 END$$
