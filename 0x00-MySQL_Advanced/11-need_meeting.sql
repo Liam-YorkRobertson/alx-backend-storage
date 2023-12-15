afd0ad6d7b546a4f51ef43b495d580068b42c1a9
“Liam-YorkRobertson” <liamyorkdeonrobertson@gmail.com>
2023-12-15 01:43:33 +0000

    SQL script that creates a view need_meeting that lists all students that have a score under 80 (strict) and no last_meeting or more than 1 month

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -1,3 +1,3 @@
 -- SQL script that creates a view need_meeting that lists all students that have a score under 80 (strict) and no last_meeting or more than 1 month
 
-CREATE VIEW need_meeting AS SELECT name FROM students WHERE score < 80 OR IFNULL(students.last_meeting, '1970-01-01') < DATE_ADD(NOW(), INTERVAL -1 MONTH);
+CREATE VIEW need_meeting AS SELECT name FROM students WHERE score < 80 AND IFNULL(students.last_meeting, '1970-01-01') < DATE_ADD(NOW(), INTERVAL -1 MONTH);
