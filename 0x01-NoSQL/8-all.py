00c6282fbb0c0787f3786431ee6c74a748ab5702
“Liam-YorkRobertson” <liamyorkdeonrobertson@gmail.com>
2023-12-20 01:49:52 +0200

    Python function that lists all documents in a collection

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+function that lists all documents in a collection
+"""
+
+
+def list_all(mongo_collection):
+    """
+    lists all documents in a collection
+    """
+    return list(mongo_collection.find())
