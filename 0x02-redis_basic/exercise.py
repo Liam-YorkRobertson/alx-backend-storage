6bd17d06c9cd977b3afaa343201ee33f76511d40
“Liam-YorkRobertson” <liamyorkdeonrobertson@gmail.com>
2023-12-20 23:50:09 +0200

    Working with redis and converting data types

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,6 +4,7 @@ Working with the basic aspects of redis
 """
 import redis
 import uuid
+from typing import Union, Callable, Optional
 
 
 class Cache:
@@ -17,10 +18,25 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data) -> str:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         """
         store input data in Redis
         """
         gen_key = str(uuid.uuid4())
         self._redis.set(gen_key, data)
         return (gen_key)
+
+    def get(self,
+            key: str,
+            fn: Optional[Callable] = None) -> Union[str, bytes, int]:
+        value = self._redis.get(key)
+        if fn is not None:
+            return fn(value)
+        else:
+            return value
+
+    def get_str(self, key: str) -> str:
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> int:
+        return self.get(key, fn=int)
