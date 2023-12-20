a85b73cedcc97c220804ff4314be4293d9e34ebe
“Liam-YorkRobertson” <liamyorkdeonrobertson@gmail.com>
2023-12-20 02:24:14 +0200

    Python function that returns the list of school having a specific topic

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+function that returns the list of school having a specific topic
+"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    returns the list of school having a specific topic
+    """
+    result_topic = mongo_collection.find({"topics": topic})
+    return list(result_topic)
