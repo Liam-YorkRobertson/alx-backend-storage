7acf74ef94d7cad1e043e4e42e06683043b2e653
“Liam-YorkRobertson” <liamyorkdeonrobertson@gmail.com>
2023-12-21 01:11:17 +0200

    function that counts method calls

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -12,8 +12,6 @@ def count_calls(method: Callable) -> Callable:
     """
     counts method calls
      """
-    counts = {}
-
     @wraps(method)
     def wrapper(self, *args, **kwargs):
         key = method.__qualname__
