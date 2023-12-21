0aa37903520eee29a0575fde444036908a76ded5
“Liam-YorkRobertson” <liamyorkdeonrobertson@gmail.com>
2023-12-21 03:17:40 +0200

    module to obtain the HTML content of a particular URL and returns it

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,30 +1,37 @@
 #!/usr/bin/env python3
-""" Redis with requests """
+"""
+obtain the HTML content of a particular URL and returns it
+"""
 import requests
 from functools import wraps
 from typing import Callable
 import redis
+from datetime import datetime, timedelta
 
 
-def count(method: Callable):
-    """ Count the call to requests """
-    r = redis.Redis()
+def count_access(func: Callable) -> Callable:
+    """
+    count call to requests
+    """
+    redis_conn = redis.Redis()
 
-    @wraps(method)
-    def wrapped(url):
-        """ function that will count """
-        r.incr(f"count:{url}")
-        expiration_count = r.get(f"cached:{url}")
-        if expiration_count:
-            return expiration_count.decode('utf-8')
-        html = method(url)
-        r.setex(f"cached:{url}", 10, html)
-        return html
+    @wraps(func)
+    def wrapper(url: str) -> str:
+        """
+        counts
+        """
+        count_key = f"count:{url}"
+        cached_key = f"cached:{url}"
+        access_count = redis_conn.incr(count_key)
+        cached_result = redis_conn.get(cached_key)
+        if cached_result:
+            return cached_result.decode('utf-8')
+        html = func(url)
+        redis_conn.setex(cached_key, 10, html)
+        return f"Access Count: {access_count}, HTML: {html}"
+    return wrapper
 
-    return wrapped
 
-
-@count
+@count_access
 def get_page(url: str) -> str:
-    """ module to obtain the HTML """
     return requests.get(url).text
