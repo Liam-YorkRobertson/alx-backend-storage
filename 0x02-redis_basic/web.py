5e6f0587a89848f865732be68d47286a7236df87
“Liam-YorkRobertson” <liamyorkdeonrobertson@gmail.com>
2023-12-21 03:15:07 +0200

    module to obtain the HTML content of a particular URL and returns it

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -6,18 +6,19 @@ import requests
 from functools import wraps
 from typing import Callable
 import redis
+from datetime import datetime, timedelta
 
 
 def count_access(func: Callable) -> Callable:
     """
-    count call to requests
+    count the call to requests
     """
     redis_conn = redis.Redis()
 
     @wraps(func)
     def wrapper(url: str) -> str:
         """
-        counts
+        function that will count and cache
         """
         count_key = f"count:{url}"
         cached_key = f"cached:{url}"
@@ -25,9 +26,15 @@ def count_access(func: Callable) -> Callable:
         cached_result = redis_conn.get(cached_key)
         if cached_result:
             return cached_result.decode('utf-8')
+        start_time = datetime.now()
         html = func(url)
+        end_time = datetime.now()
         redis_conn.setex(cached_key, 10, html)
-        return f"Access Count: {access_count}, HTML: {html}"
+        response_time = (end_time - start_time).total_seconds()
+
+        return f"Access Count: {access_count}, Response Time:
+            {response_time: .4f} seconds, HTML: {html}"
+
     return wrapper
 
 
