bb8d89d37f9d5c44827c053ed5f7658743e52036
“Liam-YorkRobertson” <liamyorkdeonrobertson@gmail.com>
2023-12-21 03:08:01 +0200

    module to obtain the HTML content of a particular URL and returns it

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""
+obtain the HTML content of a particular URL and returns it
+"""
+import requests
+from functools import wraps
+from typing import Callable
+import redis
+
+
+def count_access(func: Callable) -> Callable:
+    """
+    count call to requests
+    """
+    redis_conn = redis.Redis()
+
+    @wraps(func)
+    def wrapper(url: str) -> str:
+        """
+        counts
+        """
+        count_key = f"count:{url}"
+        cached_key = f"cached:{url}"
+        access_count = redis_conn.incr(count_key)
+        cached_result = redis_conn.get(cached_key)
+        if cached_result:
+            return cached_result.decode('utf-8')
+        html = func(url)
+        redis_conn.setex(cached_key, 10, html)
+        return f"Access Count: {access_count}, HTML: {html}"
+    return wrapper
+
+
+@count_access
+def get_page(url: str) -> str:
+    return requests.get(url).text
